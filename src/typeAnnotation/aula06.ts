68a408ee8759fbcc168cdac5cc47c2adaf3ab27a
ScriptCamilo <script.camilo@gmail.com>
2021-08-18 09:12:43 -0300

    feat: sixth class done<TypeAnnotation: Tuple>

---
+++ src/typeAnnotation/aula06.ts
@@ -0,0 +1,17 @@
+// Tuple
+const dadosCliente1: readonly [number, string] = [1, 'Script'];
+const dadosCliente2: [number, string, string] = [1, 'Script', 'Camilo'];
+const dadosCliente3: [number, string, string?] = [1, 'Script'];
+const dadosCliente4: [number, string, ...string[]] = [1, 'Scritp', 'Camilo'];
+
+console.log(dadosCliente1);
+console.log(dadosCliente2);
+console.log(dadosCliente3);
+console.log(dadosCliente4);
+
+// readonly array
+const array1: readonly string[] = ['Script', 'Camilo'];
+const array2: ReadonlyArray<string> = ['Script', 'Camilo'];
+
+console.log(array1);
+console.log(array2);
