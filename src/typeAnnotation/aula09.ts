dd7dea2364e2c57b83368f699f6bbce4f83af37b
ScriptCamilo <script.camilo@gmail.com>
2021-08-19 10:58:22 -0300

    feat: ninth class done<TypeAnnotation: enum>

---
+++ src/typeAnnotation/aula09.ts
@@ -0,0 +1,17 @@
+enum Colors {
+  VERMELHO = 10,
+  AZUL = 100,
+  AMARELO = 200,
+}
+// Ele junta os dois
+enum Colors {
+  ROXO = 'ROXO',
+  VERDE = 201,
+  ROSA, //Quando não especificado ele pega o valor anterior e soma +1
+}
+
+function colorChoice(color: Colors): void {
+  console.log(Colors[color]);
+}
+
+colorChoice(10);
