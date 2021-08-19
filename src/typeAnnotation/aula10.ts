f019b0460d9e5771dba0b005ee53f7d328236469
ScriptCamilo <script.camilo@gmail.com>
2021-08-19 11:08:43 -0300

    feat: tenth class done<TypeAnnotation: unknown>

---
+++ src/typeAnnotation/aula10.ts
@@ -0,0 +1,10 @@
+export let x: unknown;
+
+x = 100;
+x = 'Script';
+x = 900;
+x = '10';
+const y = 800;
+
+if (typeof x === 'number') console.log(x + y);
+else console.log('The type of x is not number');
