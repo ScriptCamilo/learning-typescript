05e0c8d5054b346a6642ac15de37574fc90b479b
ScriptCamilo <script.camilo@gmail.com>
2021-08-23 09:36:50 -0300

    feat: fourteenth class done<TypeAnnotation: intersection>

---
+++ src/typeAnnotation/aula14.ts
@@ -0,0 +1,19 @@
+type HasName = { name: string };
+type HasLastname = { lastname: string };
+type HasAge = { age: number };
+type Person = HasName & HasLastname & HasAge;
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type intersection = AB & AC & AD; // type intersection = "A"
+
+const person: Person = {
+  name: 'Script',
+  lastname: 'Camilo',
+  age: 22,
+};
+
+console.log(person); // { name: 'Script', lastname: 'Camilo', age: 22 }
+
+export { person };
